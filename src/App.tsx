0e327cd8942c4b72dfb90d5de5c9bd055a270c03
Adiam Ahmed <adu.adu911@gmail.com>
2025-02-27 00:44:10 -0500

    Implemented basic registration form and integrated with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css';
+import ParentProfile from './pages/ParentProfile'
 
 function App() {
   return (
-<div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="p-6 bg-white shadow-md rounded-md max-w-md">
-        <h1 className="text-3xl font-heading text-primary mb-4">
-          Welcome to WeShare.
-        </h1>
-        <p className="text-lg text-secondary">
-          This is a starter template .
-        </p>
-      </div>
-    </div>
+    <ParentProfile />
+
   );
 }
 
