7bb0b6e10bbe06f5dbb1c6b3813c0e9d060ca278
klkode <katrinalkite@gmail.com>
2025-03-11 08:26:49 -0400

    Add local state for language. Add two buttons to toggle language between English and Nepali.

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,31 @@ import SignUpPage from './pages/auth/Signup';
 // import SignupSuccess from "./components/signup/SignupSuccess";
 import LoginPage from './pages/auth/Login';
 import Dashboard from './pages/dashboard';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup/*" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+  const [language, setLanguage] = useState(localStorage.getItem('language') || 'en');
 
+  function changeLanguage(language: string):void {
+    localStorage.setItem('language', language);
+  }
+
+
+  return (
+    <main>
+      <div>
+        <button onClick={() => changeLanguage("en")}>{`EN`}</button>
+        <button onClick={() => changeLanguage("ne")}>{`NE`}</button>
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
