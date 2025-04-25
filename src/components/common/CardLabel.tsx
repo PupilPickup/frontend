48ef8f74dcdd90143bcf8cd8ac3cfc6b59daf375
klkode <katrinalkite@gmail.com>
2025-04-25 18:48:59 -0400

    Adjust styling for labels, inputs and error text to be consistent throughout site. Have labels be on same row as inputs exxcept on mobile view. Fix english text so municipality / district can naturally break line pace when required.

--- src/components/common/CardLabel.tsx
+++ src/components/common/CardLabel.tsx
@@ -9,7 +9,7 @@ const CardLabel: React.FC<CardLabelProps> = ( { label, data, className } ) => {
     return (
         <div className={`flex flex-row text-sm sm:text-base text-black m-1 p-2 space-x-2 ${className}`}>
             <p className="font-bold">{label}: </p>
-            <p className="">{data}</p>
+            <p>{data}</p>
         </div>
     );
 };
