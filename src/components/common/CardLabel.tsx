c24e3a137249974f70283158a69bbbe729875a46
klkode <katrinalkite@gmail.com>
2025-04-25 13:40:36 -0400

    style card labels to be on the same line as a row rather than a column. Make text slightly bigger for non-mobile screens.

--- src/components/common/CardLabel.tsx
+++ src/components/common/CardLabel.tsx
@@ -6,9 +6,9 @@ interface CardLabelProps {
 const CardLabel: React.FC<CardLabelProps> = ( { label, data } ) => {
 
     return (
-        <div>
-            <p className="text-sm text-gray-600 font-bold">{label}: </p>
-            <p className="text-sm text-gray-600">{data}</p>
+        <div className="flex flex-row text-sm sm:text-base text-black m-1 p-2 space-x-2">
+            <p className="font-bold">{label}: </p>
+            <p className="">{data}</p>
         </div>
     );
 };
