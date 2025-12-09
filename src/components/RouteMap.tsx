29a20d30ce42d7441d43072cfc64363cdae85b86
Logan Woodbury <lwoodbury18@gmail.com>
2025-12-09 12:46:51 -0600

    added function to retrieve child locations and add them to the routeMap

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -16,10 +16,24 @@ const RouteMap: React.FC<RouteMapProps> = ({
 }) => {
 
     const [isLoading, setIsLoading] = useState(true);
-    const [originLatitude, setOriginLatitude] = useState('');
-    const [originLongitude, setOriginLongitude] = useState('');
-    const [destinationLatitude, setDestinationLatitude] = useState('');
-    const [destinationLongitude, setDestinationLongitude] = useState('');
+    const [isRetrievingLocations, setIsRetrievingLocations] = useState(true);
+    const [childLocationsArray, setChildLocationsArray] = useState<MapCoordinate[]>([]);
+    const [originLatitude, setOriginLatitude] = useState(0);
+    const [originLongitude, setOriginLongitude] = useState(0);
+    const [destinationLatitude, setDestinationLatitude] = useState(0);
+    const [destinationLongitude, setDestinationLongitude] = useState(0);
+
+    interface ServerLocation {
+        user_id: string;
+        latitude: number;
+        longitude: number;
+    }
+
+    interface MapCoordinate {
+        lat: number,
+        lng: number,
+        key: string
+    }
 
     
     const apiUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
@@ -49,20 +63,65 @@ const RouteMap: React.FC<RouteMapProps> = ({
     }
 
     //TODO: Add function to retrieve children locations and set to destinationLatitude, destinationLongitude
+    async function getChildLocation(token:string, userName:string, userId:string){
+
+        try {
+            const response = await axios.get(`${apiUrl}/mapRoute/childLocation`, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    user_name: userName,
+                    user_id: userId,
+                },
+            });
+            const locations: ServerLocation[] = response.data.accepted_child_locations || [];
+            const coordinatesArray: MapCoordinate[] = locations.map((location: ServerLocation) => {
+                return {
+                    lat: location.latitude,
+                    lng: location.longitude,
+                    key: location.user_id
+                }
+            })
+            setChildLocationsArray(coordinatesArray);
+            setIsRetrievingLocations(true);
+        }catch(error){
+            console.error(error)
+            setIsLoading(false);
+        }
+    }
+
+    //function to add child location to the route map
+    function handleAddLocationToRoute(locationSet: MapCoordinate){
+        setDestinationLatitude(locationSet.lat);
+        setDestinationLongitude(locationSet.lng);
+        console.log(locationSet);
+        setIsRetrievingLocations(false);
+        setIsLoading(false);
+    }
 
     
     useEffect(() => {
         
         getDriverLocation(token, userName, userId);
+        getChildLocation(token, userName, userId);
 
     },[token, userId, userName]);
 
     //Google maps embed URL
     //need to add destinatinLatitude and destinationLongitude for destination instead of hardwired destination
-    const mapsApiUrl = `https://www.google.com/maps/embed/v1/directions?key=${mapsApiKey}&origin=${originLatitude},${originLongitude}&destination=place_id:ChIJV0AwM30rDogR2sd-X0cgErU`;
+    const mapsApiUrl = `https://www.google.com/maps/embed/v1/directions?key=${mapsApiKey}&origin=${originLatitude},${originLongitude}&destination=${destinationLatitude},${destinationLongitude}`;
     
     if(isLoading) {
         return <p>...Loading Best Route</p>
+    }
+
+    if(isRetrievingLocations){
+        return (
+            <>
+                {childLocationsArray.map((item, index) => (
+                    <button onClick={() => handleAddLocationToRoute(item)}>Add child {item.key} to route</button>
+                ))}
+            </>
+        )
     }
 
     return (
